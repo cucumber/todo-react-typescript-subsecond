11f62b4a795475679a0c272f8670417844f73a7b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-03-16 11:25:51 +0000

    Make todoList type explicit

--- features/step_definitions/todo_steps.ts
+++ features/step_definitions/todo_steps.ts
@@ -1,25 +1,28 @@
 import { Before, Given, When, Then } from 'cucumber'
 import assert from 'assert'
 import TodoList from '../../src/TodoList'
+import ITodoList from '../../src/ITodoList'
+
+let todoList: ITodoList
 
 Before(function() {
-  this.todoList = new TodoList()
+  todoList = new TodoList()
 })
 
 Given('there is already {int} todo', function(todoCount: number) {
   for (let n = 0; n < todoCount; n++) {
-    this.todoList.add(`TODO #${n + 1}`)
+    todoList.add(`TODO #${n + 1}`)
   }
 })
 
 When('I add {string}', function(todo: string) {
-  this.todoList.add(todo)
+  todoList.add(todo)
 })
 
 Then('the text of the 2nd todo should be {string}', function(
   expectedTodo: string
 ) {
   // Write code here that turns the phrase above into concrete actions
-  const secondTodo = this.todoList.getTodos()[1]
+  const secondTodo = todoList.getTodos()[1]
   assert.strictEqual(secondTodo, expectedTodo)
 })
