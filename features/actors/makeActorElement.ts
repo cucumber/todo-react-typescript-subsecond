dcb9fad7c352723a379f93c957bddc84c8d2c4e8
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2020-03-20 00:08:32 +0000

    Use CSS classes unlikely to clash

--- features/actors/makeActorElement.ts
+++ features/actors/makeActorElement.ts
@@ -6,7 +6,7 @@ export default async function makeActorElement(name: string): Promise<HTMLElemen
   const div = document.createElement('div')
   div.innerHTML = await readFile(__dirname + '/fake-browser.html', { encoding: 'utf-8' })
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  div.querySelector('span.title')!.innerHTML = name
+  div.querySelector('span.cucumber-electron-fake-browser-title')!.innerHTML = name
   document.body.appendChild(div)
-  return div.querySelector('div.content')! as HTMLElement
+  return div.querySelector('div.cucumber-electron-fake-browser-content')! as HTMLElement
 }
